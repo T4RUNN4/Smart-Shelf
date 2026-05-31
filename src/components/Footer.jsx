59ba8cf6402fc6830223232ade1bf2b6f98d5cd8
Tarunna Sen <tarunnasen@gmail.com>
2026-05-31 19:49:47 +0600

    Portfolio linked in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,19 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
     <footer className="footer sm:footer-horizontal footer-center bg-base-300 p-4 text-sm text-center py-8 text-black/50">
       <aside>
         <p>
           Copyright © {new Date().getFullYear()} - All right reserved |
-          Developed By Tarunna Sen
+          Developed By{" "}
+          <Link
+            className="text-[#738f6d]"
+            href="https://tarunna-sen.vercel.app"
+            target="_blank"
+          >
+            Tarunna Sen
+          </Link>
         </p>
       </aside>
     </footer>
